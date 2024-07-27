3eedfd90af6879da6d6c2f0e587010621a76b9f8
zihyeon08 <104641422+zihyeon08@users.noreply.github.com>
2024-07-28 01:30:41 +0900

    Add GitHub Actions workflow for Azure deployment

--- workshop/AspireYouTubeSummariser.AppHost/Program.cs
+++ workshop/AspireYouTubeSummariser.AppHost/Program.cs
@@ -4,18 +4,15 @@ var cache = builder.AddRedis("cache");
 
 var config = builder.Configuration;
 
-
 var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp")
-                     .WithExternalHttpEndpoints()
-                     .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
-                     .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
-                     .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
-
+                    .WithExternalHttpEndpoints()
+                    .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
+                    .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
+                    .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
 
 builder.AddProject<Projects.AspireYouTubeSummariser_WebApp>("webapp")
        .WithExternalHttpEndpoints()
        .WithReference(cache)
        .WithReference(apiapp);
-
 
 builder.Build().Run();
